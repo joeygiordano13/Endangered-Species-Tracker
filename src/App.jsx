6a56367bbb12c90ffdfccc49709596b96b434033
Joey Giordano <joeydanodano@gmail.com>
2022-07-11 11:45:44 -0400

    converted app to functional component for readability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default class App extends React.Component {
-    constructor(props) {
-        super(props);
+function App() {
+    const [ uid, setUid] = useState('');
+    const [ speciesName, setSpeciesName ] = useState('');
+    const [ roundedGRank, setRoundedGRank ] = useState('');
 
-        this.state = {
-            uid: null,
-            speciesName: null,
-            roundedGRank: null
-        }
+    const parseData = (data) => {
+        setUid(data.elementGlobalId);
+        setSpeciesName(data.primaryCommonName);
+        setRoundedGRank(data.roundedGRank);
     }
 
-    componentDidMount() {
+    useEffect(() => {
         // Hardcoded UID
-        const dummyuid = 'ELEMENT_GLOBAL.2.154701';
-        this.setState( { uid: dummyuid } );
-        // GET request with fetch, convert to async function
-        fetch(`https://explorer.natureserve.org//api/data/taxon/${dummyuid}`)
+        const dummyuid = '154701';
+        // Async function to get info from natureserve api
+        const fetchData = async () => {
+            const res = await fetch(`https://explorer.natureserve.org//api/data/taxon/ELEMENT_GLOBAL.2.${dummyuid}`)
             .then(response => response.json())
-            .then(data => this.setState( { speciesName: data.primaryCommonName, 
-                                           roundedGRank: data.roundedGRank }))
-    }
-
-
-    render() {
-        const { speciesName } = this.state;
-        const { roundedGRank } = this.state;
-        const { uid } = this.state;
-        // Colors for GRank: Red -> Blue -> Green
-        const palette = ['red', 'purple', 'blue', 'teal', 'green'];
-        
-        // TODO: error handle for extinct species (add new color?)
-        // Replace this code once request is fixed
-        let color = 'white';
-        if (roundedGRank != null) {
-            const num = roundedGRank.slice(1);
-            color = palette[num-1];
+            .then(data => parseData(data));
+            return res;
         }
+        fetchData().catch(console.error)
+    }, []);
 
-        return (
-            <div className='text-center'>
-                <h2>Species Tracker:</h2>
-                <div>UID: {uid}</div>
-                <div>Name: {speciesName}</div>
-                <div>G rank: {roundedGRank}</div>
-                <div style={{color: color}}>{roundedGRank}</div>
-            </div>
-        );
+    // Colors for GRank: Red -> Blue -> Green
+    const palette = ['red', 'purple', 'blue', 'teal', 'green'];
+        
+    // TODO: error handle for extinct species (add new color?)
+    // Replace this code once request is fixed
+    let color = 'white';
+    if (roundedGRank != null) {
+        const num = roundedGRank.slice(1);
+        color = palette[num-1];
     }
+
+    return (
+        <div className='text-center'>
+            <h2>Species Tracker:</h2>
+            <div>UID: {uid}</div>
+            <div>Name: {speciesName}</div>
+            <div>G rank: {roundedGRank}</div>
+            <div style={{color: color}}>{roundedGRank}</div>
+        </div>
+    );
 }
+
+export default App;
